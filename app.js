2658d7230fab823b2f9d21ddb910d2d7b1db66ee
Marguerite Brugger <margueritebrugger@Marguerites-MacBook-Air.local>
2021-08-02 23:52:48 -0500

    switched to json authentication

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ require('dotenv').config();
 
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 const db = admin.firestore();
 
