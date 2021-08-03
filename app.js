c793f128ac3084e5e72339a1e493d08cfa426306
Marguerite Brugger <margueritebrugger@Marguerites-MacBook-Air.local>
2021-08-02 23:07:54 -0500

    added firebase config for heroku

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ require('dotenv').config();
 
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+admin.initializeApp();
 
 const db = admin.firestore();
 
